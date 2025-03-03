e8808d80f1c0474ab9f5d29c08032480323f5a01
CamilaBasualdoCibils <camila.basualdocibils@gmail.com>
2025-03-02 16:53:52 -0800

    window displays now

--- src/Entry.cpp
+++ src/Entry.cpp
@@ -1,5 +1,5 @@
 
-#include <GL/glew.h>
+//#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 GLFWwindow *window;
@@ -17,10 +17,12 @@ int main()
     glfwSetErrorCallback(ERROR_CALLBACK);
     monitor = glfwGetPrimaryMonitor();
     video_mode = glfwGetVideoMode(monitor);
-    glfwWindowHint(GLFW_CONTEXT_CREATION_API, GLFW_EGL_CONTEXT_API); // Use EGL
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);             // Request OpenGL ES
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 1);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+   // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+   glfwWindowHint(GLFW_CONTEXT_CREATION_API, GLFW_EGL_CONTEXT_API);
+glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
     window = glfwCreateWindow(video_mode->width, video_mode->height, "Barton Display", nullptr, nullptr);
     std::cout << "hello\n";
 
