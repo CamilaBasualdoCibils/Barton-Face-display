38c138f6b26458dc9f5d86840e73423326c14418
CamilaBasualdoCibils <camila.basualdocibils@gmail.com>
2025-03-04 10:59:58 -0800

    Resolution rounds to power of 2

--- src/Entry.cpp
+++ src/Entry.cpp
@@ -24,14 +24,10 @@ uint8_t quad_indicies[] = {
     2, 3, 0  // Second triangle: bottom-right, bottom-left, top-left
 };
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-    float resolution_scale = RESOLUTION_SCALE;
-    if (argc >=2) {
-        resolution_scale = std::stof(argv[1]);
-    }
 
-    Window::Init({480,320},
+    Window::Init({480, 320},
                  WindowHints::eDECORATED * DECORATED |
                      WindowHints::eRESIZEABLE * RESIZEABLE
 #ifdef _RELEASE
@@ -42,12 +38,11 @@ int main(int argc, char** argv)
     std::shared_ptr<Window> window = Window::GetInstance();
 
     std::shared_ptr<Shader> face_shader = Shader::FromFiles("face.vert", "face.frag");
-    //face_shader->BindAttribLocation("a_position", 0);
-    //face_shader->BindAttribLocation("a_texcoord", 1);
-    std::shared_ptr<Shader> resize_shader = Shader::FromFiles("Project.vert","Project.frag");
+    // face_shader->BindAttribLocation("a_position", 0);
+    // face_shader->BindAttribLocation("a_texcoord", 1);
+    std::shared_ptr<Shader> resize_shader = Shader::FromFiles("Project.vert", "Project.frag");
 
     glReleaseShaderCompiler(); // NO MORE SHADER COMPILES AFTER THIS <========
-
 
     const std::vector<VertexArrayAttrib> attribs = {{GL_FLOAT, 2, false}, {GL_FLOAT, 2, false}};
 
@@ -60,7 +55,31 @@ int main(int argc, char** argv)
 
     auto lastTime = std::chrono::high_resolution_clock::now();
     int frameCount = 0;
-    std::shared_ptr<Framebuffer_Low> render_fbo = std::make_shared<Framebuffer_Low>((vec2)window->Size()* resolution_scale);
+
+    float resolution_scale = RESOLUTION_SCALE;
+    if (argc >= 2)
+    {
+        resolution_scale = std::stof(argv[1]);
+    }
+    uvec2 final_resolution = (vec2)window->Size() * resolution_scale;
+    bool round_to_pow_2 = argc >= 3 ? std::stoi(argv[2]) : 0;
+    if (round_to_pow_2 || argc < 3)
+    {
+        std::cout << "Rounding resolution to power of 2\n";
+        for (int i = 0; i < 2; i++)
+        {
+            float n = final_resolution[i];
+            if (n <= 0)
+                final_resolution[i] = 1; // Edge case: return 1 if n is 0 or negative
+
+            int lower = 1 << (int)std::floor(std::log2(n));
+            int upper = 1 << (int)std::ceil(std::log2(n));
+
+            final_resolution[i] = (n - lower < upper - n) ? lower : upper;
+        }
+    }
+    std::cout << "Upscale Resolution " << final_resolution.x << ", " << final_resolution.y << std::endl;
+    std::shared_ptr<Framebuffer_Low> render_fbo = std::make_shared<Framebuffer_Low>(final_resolution);
 
     while (!window->ShouldClose() && window->GetKey(GLFW_KEY_ESCAPE) != GLFW_PRESS)
     {
@@ -69,7 +88,7 @@ int main(int argc, char** argv)
 
         render_fbo->Use();
         face_shader->Use();
-        glUniform1f(0,(float)glfwGetTime());
+        glUniform1f(0, (float)glfwGetTime());
         vert_array->Draw(6);
 
         // If 1 second has passed, calculate and print framerate
@@ -81,8 +100,8 @@ int main(int argc, char** argv)
             lastTime = currentTime;
         }
 
-        glViewport(0,0,window->Size().x,window->Size().y);
-        glBindFramebuffer(GL_FRAMEBUFFER,0);
+        glViewport(0, 0, window->Size().x, window->Size().y);
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
         resize_shader->Use();
         glBindTexture(GL_TEXTURE_2D, render_fbo->GetTexture());
         vert_array->Draw(6);
