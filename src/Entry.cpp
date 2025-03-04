c100014a8f4e53a83983ef798f5884e488e5032b
CamilaBasualdoCibils <camila.basualdocibils@gmail.com>
2025-03-03 16:06:49 -0800

    uint to uchar for indicies

--- src/Entry.cpp
+++ src/Entry.cpp
@@ -26,14 +26,14 @@ int main()
     { 1, -1},      {1.0f, 0.0f}, // Bottom-right
     {-1, -1},      {0.0f, 0.0f}  // Bottom-left
     };
-    unsigned int quad_indicies[] = {
+    uint8_t quad_indicies[] = {
         0, 1, 2,  // First triangle: top-left, top-right, bottom-right
         2, 3, 0   // Second triangle: bottom-right, bottom-left, top-left
     };
     const std::vector<VertexArrayAttrib> attribs = {{GL_FLOAT,2,false},{GL_FLOAT,2,false}};
     std::shared_ptr<VertexArray> vert_array = 
     std::make_shared<VertexArray>((void*)quad_data,sizeof(quad_data),attribs) ;
-    vert_array->SetElementBuffer((void*)quad_indicies,sizeof(quad_indicies),GL_UNSIGNED_INT);
+    vert_array->SetElementBuffer((void*)quad_indicies,sizeof(quad_indicies),GL_UNSIGNED_BYTE);
     OpenGL::GetInstance()->DepthTest(false);
 
     auto lastTime = std::chrono::high_resolution_clock::now();
